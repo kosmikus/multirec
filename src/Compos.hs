862bc3cd3812ad43bf517c6b8d6592cb53546f9b
stefan <stefan@51733d7a-322c-11dd-8ca2-af992726123e>
2008-07-02 13:40:46 +0000

    * moved transform to its own module
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/MutualRec/trunk@168 51733d7a-322c-11dd-8ca2-af992726123e

--- src/Compos.hs
+++ src/Compos.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes       #-}
-{-# LANGUAGE ScopedTypeVariables#-}
 
 module Compos where
 
@@ -10,16 +9,4 @@ import HMap
 composOp :: (Ix l ix, HMap (PF l)) =>
             (forall ix. Ix l ix => l ix -> ix -> ix) -> ix -> ix
 composOp f = to . hmap f . from
-
--- this function is painful to define for various reasons
--- * composOp needs an index consuming argument so we would need
---   to write "const (transform f)", but this makes a function of
---   type |forall ix. Ix l ix => l1 ix -> ix -> ix
--- * So we are forced to associate the two with a type signature.
-transform :: forall l ix . (Ix l ix, HMap (PF l)) =>
-             (forall ix. Ix l ix => l ix -> ix -> ix) -> ix -> ix
-transform f = f ix . composOp helper
-  where
-    helper :: forall ix. Ix l ix => l ix -> ix -> ix
-    helper w = transform f
 
