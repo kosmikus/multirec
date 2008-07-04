cfa3c3f32a145d74a00878f7c7784975edf07c01
stefan <stefan@51733d7a-322c-11dd-8ca2-af992726123e>
2008-07-04 13:41:28 +0000

    * implemented next
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/MutualRec/trunk@201 51733d7a-322c-11dd-8ca2-af992726123e

--- src/Zipper.hs
+++ src/Zipper.hs
@@ -48,23 +48,30 @@ class Zipper_ f where
   first :: (forall b. Ix l b => b -> Ctx f l ix b -> a)
         -> f l ix -> Maybe a
   fill  :: Ix l b => Ctx f l ix b -> b -> f l ix
+  next  :: (forall b. Ix l b => b -> Ctx f l ix b -> a)
+        -> Ix l b => Ctx f l ix b -> b -> Maybe a
 
 class (Ix l ix, Zipper_ (PF l)) => Zipper l ix
 
 instance Zipper_ (Id xi) where
-  first f (Id x)   = return (f x (CId Refl))
-  fill (CId prf) x = castId prf Id x
+  first f (Id x)     = return (f x (CId Refl))
+  fill (CId prf) x   = castId prf Id x
+  next f (CId prf) x = Nothing 
 
 instance Zipper_ (K a) where
-  first f (K a)    = Nothing
-  fill (CK void) x = refute void
+  first f (K a)      = Nothing
+  fill (CK void) x   = refute void
+  next f (CK void) x = Nothing
 
 instance (Zipper_ f, Zipper_ g) => Zipper_ (f :+: g) where
-  first f (L x)           = first (\z -> f z . CSum . Left) x
-  first f (R y)           = first (\z -> f z . CSum . Right) y
+  first f (L x)             = first (\z -> f z . CSum . Left ) x
+  first f (R y)             = first (\z -> f z . CSum . Right) y
   
-  fill (CSum (Left  c)) x = L (fill c x)
-  fill (CSum (Right c)) y = R (fill c y)
+  fill (CSum (Left  c)) x   = L (fill c x)
+  fill (CSum (Right c)) y   = R (fill c y)
+
+  next f (CSum (Left  c)) x = next (\z -> f z . CSum . Left ) c x
+  next f (CSum (Right c)) y = next (\z -> f z . CSum . Right) c y
 
 instance (Zipper_ f, Zipper_ g) => Zipper_ (f :*: g) where
   first f (x :*: y) =
@@ -74,9 +81,17 @@ instance (Zipper_ f, Zipper_ g) => Zipper_ (f :*: g) where
   fill (CProd (Left  (c, y))) x = fill c x :*: y
   fill (CProd (Right (x, c))) y = x :*: fill c y
 
+  next f (CProd (Left  (c, y))) x =
+    next (\z c' -> f z (CProd (Left (c', y)))) c x `mplus`
+    first (\z c' -> f z (CProd (Right (fill c x, c')))) y
+
+  next f (CProd (Right (x, c))) y =
+    next (\z c' -> f z (CProd (Right (x, c')))) c y
+
 instance Zipper_ f => Zipper_ (f ::: xi) where
-  first f (Tag x)     = first (\z -> f z . CTag Refl) x
-  fill (CTag prf c) x = castTag prf Tag (fill c x)
+  first f (Tag x)       = first (\z -> f z . CTag Refl) x
+  fill (CTag prf c) x   = castTag prf Tag (fill c x)
+  next f (CTag prf c) x = next (\z -> f z . CTag prf) c x
 
 -- helpers
 castId  :: (b :=: xi)
