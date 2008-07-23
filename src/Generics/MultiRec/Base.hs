8d8a3a2576a447e5f21402be02da75c228d4c22a
andres <andres@51733d7a-322c-11dd-8ca2-af992726123e>
2008-07-23 15:16:29 +0000

    * Added documentation.
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/MutualRec/trunk@290 51733d7a-322c-11dd-8ca2-af992726123e

--- src/Generics/MultiRec/Base.hs
+++ src/Generics/MultiRec/Base.hs
@@ -6,7 +6,21 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE Rank2Types            #-}
 
-module Generics.MultiRec.Base where
+module Generics.MultiRec.Base 
+  (-- * Structure types
+   I(..), unI,
+   K(..), (:+:)(..), (:*:)(..),
+   (:>:)(..), unTag,
+
+   -- ** Unlifted variants
+   I0(..), K0(..),
+
+   -- * Indexed systems
+   PF, Str, Ix(..),
+
+   -- * Higher-order fixed points
+   HFix(..)
+  ) where
 
 import Control.Applicative
 
@@ -16,23 +30,39 @@ infixr 5 :+:
 infix  6 :>:
 infixr 7 :*:
 
+-- | Represents recursive positions. The first argument indicates
+-- which type (within the system) to recurse on.
 data I :: * -> (* -> *) -> (* -> *) -> * -> * where
   I :: Ix s xi => r xi -> I xi s r ix
 
+-- | Destructor for 'I'.
 unI :: I xi s r ix -> r xi
 unI (I x) = x
 
+-- | Represents constant types that do not belong to the system.
 data K a       (s :: * -> *) (r :: * -> *) ix = K {unK :: a}
 
+-- | Represents sums (choices between constructors).
 data (f :+: g) (s :: * -> *) (r :: * -> *) ix = L (f s r ix) | R (g s r ix)
+
+-- | Represents products (sequences of fields of a constructor).
 data (f :*: g) (s :: * -> *) (r :: * -> *) ix = f s r ix :*: g s r ix
 
+-- | Is used to indicate the type (within the system) that a
+-- particular constructor injects to.
 data (:>:) :: ((* -> *) -> (* -> *) -> * -> *) -> * -> (* -> *) -> (* -> *) -> * -> * where
-  Tag { unTag :: f s r ix } :: (f :>: ix) s r ix
+  Tag :: f s r ix -> (f :>: ix) s r ix
+
+-- | Destructor for '(:>:)'.
+unTag :: (f :>: ix) s r ix -> f s r ix
+unTag (Tag x) = x
 
 -- ** Unlifted variants
 
+-- | Unlifted version of 'I'.
 newtype I0 a   = I0 { unI0 :: a }
+
+-- | Unlifted version of 'K'.
 newtype K0 a b = K0 { unK0 :: a }
 
 instance Functor I0 where
@@ -42,16 +72,17 @@ instance Applicative I0 where
   pure              = I0
   (I0 f) <*> (I0 x) = I0 (f x)
 
--- * Indexed families
+-- * Indexed systems
 
+-- | Type family describing the pattern functor of a system.
 type family PF s :: (* -> *) -> (* -> *) -> * -> *
 type Str s ix = (PF s) s I0 ix
 
--- | Some functions need to have their types desugared in order to make programs
--- that use them typable.  Desugaring consists in transforming ``inline'' type
--- family applications into equality constraints. This is a strangeness in current
--- versions of GHC that hopefully will be fixed sometime in the future.
 class Ix s ix where
+  -- | Some functions need to have their types desugared in order to make programs
+  -- that use them typable.  Desugaring consists in transforming ``inline'' type
+  -- family applications into equality constraints. This is a strangeness in current
+  -- versions of GHC that hopefully will be fixed sometime in the future.
   from  :: (pfs ~ PF s) => ix -> pfs s I0 ix
   to    :: (pfs ~ PF s) => pfs s I0 ix -> ix
   index :: s ix
