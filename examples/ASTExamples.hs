1d6071ecb2d242114b7515e112e480226d3ddd24
andres <andres@51733d7a-322c-11dd-8ca2-af992726123e>
2008-08-22 08:34:42 +0000

    * added another example file
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/MutualRec/trunk@364 51733d7a-322c-11dd-8ca2-af992726123e

---
+++ examples/ASTExamples.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies     #-}
+
+module ASTExamples where
+
+import Data.Maybe (fromJust)
+import Control.Arrow ((>>>))
+import Control.Monad ((>=>))
+
+import AST
+
+import Generics.MultiRec.Base
+import Generics.MultiRec.Compos
+import Generics.MultiRec.Fold
+
+-- | Example expression
+
+example = Let ("x" := Mul (Const 6) (Const 9))
+              (Add (EVar "x") (EVar "y"))
+
+-- | Renaming variables using 'compos'
+
+renameVar :: Expr -> Expr
+renameVar = renameVar' Expr
+  where
+    renameVar' :: Ix AST a => AST a -> a -> a
+    renameVar' Var x = x ++ "_"
+    renameVar' _   x = compos renameVar' x
+
+-- | Test for 'renameVar'
+
+testRename :: Expr
+testRename = renameVar example
+
+-- | Result of evaluating an expression
+
+data family Value aT :: *
+data instance Value Expr  =  EV  (Env -> Int)
+data instance Value Decl  =  DV  (Env -> Env)
+data instance Value Var   =  VV  Var
+
+type Env = [(Var, Int)]
+
+-- | Algebra for evaluating an expression
+
+evalAlgebra :: Algebra AST Value
+evalAlgebra _ =  
+     tag (\ (K x)                   -> EV (const x))
+  &  tag (\ (I (EV x) :*: I (EV y)) -> EV (\ env -> x env  +  y env))
+  &  tag (\ (I (EV x) :*: I (EV y)) -> EV (\ env -> x env  *  y env))
+  &  tag (\ (I (VV x))              -> EV (fromJust . lookup x))
+  &  tag (\ (I (DV e) :*: I (EV x)) -> EV (\ env -> x (e env)))
+  &  tag (\ (I (VV x) :*: I (EV v)) -> DV (\ env -> (x, v env) : env ))
+  &  tag (\ (I (DV f) :*: I (DV g)) -> DV (g . f))
+  &  tag (\ (K x)                   -> VV x)
+
+-- | Evaluator
+
+eval :: Expr -> Env -> Int
+eval x = let (EV f) = fold evalAlgebra x in f
+
+-- | Test for 'eval'
+
+testEval :: Int
+testEval = eval example [("y", -12)] 
+
