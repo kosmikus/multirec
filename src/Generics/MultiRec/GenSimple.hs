5e908a9d3bf69b41b0ac524b6ee9ee69884f8060
ewhessel <ewhessel@51733d7a-322c-11dd-8ca2-af992726123e>
2009-01-07 15:42:12 +0000

    Cleaned up code and added a list example.
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/multirec/branches/Erik@619 51733d7a-322c-11dd-8ca2-af992726123e

--- src/Generics/MultiRec/GenSimple.hs
+++ src/Generics/MultiRec/GenSimple.hs
@@ -11,9 +11,6 @@ module Simple where
 
 import Data.Char (ord)
 
--- The datatype I want to model as an example of a datatype with more than one type param
-data Tree a b = Leaf a | Branch (Tree a b) b (Tree a b) deriving Show
-
 data K a       (es :: * -> * -> *) (r :: *) = K a
 data E a       (es :: * -> * -> *) (r :: *) where
     E :: (es a e) -> e -> E a es r
@@ -28,6 +25,9 @@ infixr 6 :+:
 data Zero
 data Suc a
 
+data ParamS1 :: * -> * -> * -> * where
+    Param :: ParamS1 a Zero a
+
 data ParamS2 :: * -> * -> * -> * -> * where
     Param1 :: ParamS2 a b Zero       a
     Param2 :: ParamS2 a b (Suc Zero) b
@@ -37,12 +37,6 @@ type family PF a :: (* -> * -> *) -> * -> *
 class EIx es a where
     from :: a -> PF a es a
     to :: PF a es a -> a
-
-type instance (PF (Tree a b)) = E Zero :+: I :*: E (Suc Zero) :*: I
-
-instance EIx (ParamS2 a b) (Tree a b) where
-    from = fromTree
-    to = toTree
 
 class GMap (f :: (* -> * -> *) -> * -> *) where
     -- Dit werkt niet, want de laatste index van 'f' (de pattern functor) kan niet veranderen, en dus ook de parameters niet.
@@ -64,34 +58,49 @@ instance (GMap f, GMap g) => GMap (f :+: g) where
 instance (GMap f, GMap g) => GMap (f :*: g) where
     gmap' f g (x :*: y) = gmap' f g x :*: gmap' f g y
 
+gmap :: (EIx es a, EIx es2 b, GMap (PF a), PF a ~ PF b) => (forall n. E n es a -> E n es2 b) -> a -> b
+gmap f = to . gmap' f (gmap f) . from
+
+-- Two datatypes (list and tree) to test with, with 1 and 2 type
+-- parameters.
+type instance (PF [a]) = K () :+: E Zero :*: I
+
+fromList [] = L (K ())
+fromList (x : xs) = R (E Param x :*: I xs)
+toList :: PF [a] (ParamS1 a) [a] -> [a]
+toList (L (K ())) = []
+toList (R (E Param x :*: I xs)) = x : xs
+
+instance EIx (ParamS1 a) [a] where
+    from = fromList
+    to = toList
+
+f3 :: E n (ParamS1 Char) [Char] -> E n (ParamS1 Int) [Int]
+f3 (E Param c) = E Param (ord c)
+
+listTest = gmap f3 "test"
+
+data Tree a b = Leaf a | Branch (Tree a b) b (Tree a b) deriving Show
+
+type instance (PF (Tree a b)) = E Zero :+: I :*: E (Suc Zero) :*: I
+
 fromTree (Leaf a) = L (E Param1 a)
 fromTree (Branch l b r) = R (I l :*: E Param2 b :*: I r)
 toTree :: PF (Tree a b) (ParamS2 a b) (Tree a b) -> Tree a b
 toTree (L (E Param1 a)) = Leaf a
 toTree (R (I l :*: E Param2 b :*: I r)) = Branch l b r
 
-{-
-f :: ParamS2 Char Bool c -> c -> (ParamS2 Char Bool c, c)
-f Param1 c = (Param1, ord c)
-f Param2 b = (Param2, if b then "yes" else "no")
--}
+instance EIx (ParamS2 a b) (Tree a b) where
+    from = fromTree
+    to = toTree
 
 testTree = Branch (Leaf 'a') True (Branch (Leaf 'b') False (Leaf 'c'))
-
-{-
-f :: ParamS2 Char Bool c -> c -> E (ParamS2 Int String) r
-f Param1 c = E Param1 (ord c)
-f Param2 b = E Param2 (if b then "yes" else "no")
--}
 
 f2 :: E n (ParamS2 Char Bool) (Tree Char Bool) -> E n (ParamS2 Int String) (Tree Int String)
 f2 (E Param1 c) = E Param1 (ord c)
 f2 (E Param2 b) = E Param2 (if b then "yes" else "no")
 
-gmap :: (EIx es a, EIx es2 b, GMap (PF a), PF a ~ PF b) => (forall n. E n es a -> E n es2 b) -> a -> b
-gmap f = to . gmap' f (gmap f) . from
-
-res = gmap f2 testTree
+treeTest = gmap f2 testTree
 
 -- Dit werkt niet meer, vanwege het eerste type-argument van E.
 {-
@@ -99,7 +108,5 @@ f3 :: E n (ParamS2 Char Bool) (Tree Char Bool) -> E n (ParamS2 String Int) (Tree
 f3 (E Param1 c) = E Param2 (ord c)
 f3 (E Param2 b) = E Param1 (if b then "yes" else "no")
 
-res2 = toTree $ gmap f3 g $ fromTree testTree
-  where
-    g = toTree . gmap f3 g . fromTree
+treeTestFail = gmap f3 testTree
 -}
