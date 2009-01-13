13b6f3972a84d3294413444e29e38f694e400520
andres <andres@51733d7a-322c-11dd-8ca2-af992726123e>
2009-01-13 14:24:01 +0000

    * Added HFix.
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/multirec/trunk@633 51733d7a-322c-11dd-8ca2-af992726123e

--- src/Generics/MultiRec/HFix.hs
+++ src/Generics/MultiRec/HFix.hs
@@ -2,6 +2,22 @@
 {-# LANGUAGE TypeFamilies     #-}
 {-# LANGUAGE FlexibleContexts #-}
 
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Generics.MultiRec.HFix
+-- Copyright   :  (c) 2008--2009 Universiteit Utrecht
+-- License     :  BSD3
+--
+-- Maintainer  :  generics@haskell.org
+-- Stability   :  experimental
+-- Portability :  non-portable
+--
+-- Higher-order fixed point operator as well as conversion functions.
+-- It is rarely necessary to use 'HFix'. Generic functions
+-- usually convert between the original datatype and the functor directly.
+--
+-----------------------------------------------------------------------------
+
 module Generics.MultiRec.HFix where
 
 import Generics.MultiRec.Base
