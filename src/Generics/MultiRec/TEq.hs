b8ea429c303b3eecf996d8af7a39203bad4f6d22
andres <andres@51733d7a-322c-11dd-8ca2-af992726123e>
2009-02-06 13:43:16 +0000

    Added missing TEq module.
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/multirec/trunk@702 51733d7a-322c-11dd-8ca2-af992726123e

---
+++ src/Generics/MultiRec/TEq.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators  #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Generics.MultiRec.TEq
+-- Copyright   :  (c) 2008--2009 Universiteit Utrecht
+-- License     :  BSD3
+--
+-- Maintainer  :  generics@haskell.org
+-- Stability   :  experimental
+-- Portability :  non-portable
+--
+-- Type-level equality. This module is currently provided by the multirec
+-- library, even though it is more general and does not really belong here.
+-- 
+-----------------------------------------------------------------------------
+module Generics.MultiRec.TEq where
+
+infix 4 :=:
+
+data (:=:) :: * -> * -> * where
+  Refl :: a :=: a
+
+cast :: a :=: b -> a -> b
+cast Refl x = x
