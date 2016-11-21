edf749cc175577ddb631d744a651436c01fc7b26
Andres Loeh <andres@well-typed.com>
2016-11-21 15:22:23 +0100

    Add some documentation to example.

--- examples/GRose.hs
+++ examples/GRose.hs
@@ -18,3 +18,30 @@ data GRoseF :: * -> * -> * where
   GRose :: GRoseF a (GRose a)
 
 $(deriveAll ''GRoseF)
+
+-- Desired output:
+--
+-- data Leaf
+-- data Node
+--
+-- instance Constructor Leaf where
+--   conName _ = "Leaf"
+-- instance Constructor Node where
+--   conName _ = "Node"
+--
+-- type instance PF (GRoseF a) =
+--   (:>:) ((:+:) (C Leaf (K a)) (C Node ((:.:) [] (I (GRose a))))) (GRose a)
+--
+-- instance El (GRoseF a) (GRose a) where
+--   proof = GRose
+--
+-- instance Fam (GRoseF a) where
+--
+--   from GRose (Leaf f0) = Tag (L (C (K f0)))
+--   from GRose (Node f0) = Tag (R (C ((D . (fmap (I . I0))) f0)))
+--
+--   to GRose (Tag (L (C f0))) = Leaf (unK f0)
+--   to GRose (Tag (R (C f0))) = Node (((fmap (unI0 . unI)) . unD) f0)
+--
+-- instance EqS (GRoseF a) where
+--   eqS GRose GRose = Just Refl
